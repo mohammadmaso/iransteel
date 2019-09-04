25cdee47289c8e7d0bfc2947243ea36177cd994b
mohammad <m.mh.itg@gmail.com>
2019-09-04 08:11:28 +0430

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,7 @@ const path = require('path');
     },
    module: {
      rules: [
-        {
-            "test": /\.js$/,
-            "exclude": /node_modules/,
-            "use": {
-                "loader": "babel-loader",
-                "options": {
-                    "presets": [
-                        "env"
-                    ]
-                }
-            }
-        },
+        
        {
          test: /\.css$/,
          use: [
