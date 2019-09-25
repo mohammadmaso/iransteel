552bc78c93dabedbdcb5aaefda793c46653065b3
mohammad <m.mh.itg@gmail.com>
2019-09-25 14:43:38 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 $('#BasketPopover').popover();
+
+var timer = 60;
+
+var interval = setInterval(function() {
+    timer--;
+    $('.timer').text(timer);
+    if (timer === 0) clearInterval(interval);
+}, 1000);
