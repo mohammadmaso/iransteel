83d25b39b829bd74de6b7bc51eeca3d580266812
mohammad <m.mh.itg@gmail.com>
2019-09-26 09:30:12 +0330

    add manifest & web app

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,33 @@ var interval = setInterval(function() {
     $('.timer').text(timer);
     if (timer === 0) clearInterval(interval);
 }, 1000);
+
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
+
+  
+
+});
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+
