23b176df1572863a64106e3a2b0502a3e57bc83b
mohammad <m.mh.itg@gmail.com>
2019-10-09 09:03:13 +0330

    mamd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
-$('#BasketPopover').popover();
+//popover
+//$('#BasketPopover').popover();
 
+$(document).ready(function(){
+    $('#BasketPopover').popover({
+        placement : 'bottom',
+        html : true,
+        title : 'سبد خرید شما',
+        content : '<ul><li>محصول شماره یک </li><li>محصول شماره دو </li><li>محصول سماره سه</li><li>مشحصول شماره چهار</li></ul>'
+		
+		
+    });
+    $(document).on("click", ".popover .close" , function(){
+        $(this).parents(".popover").popover('hide');
+    });
+});
+
+
+
+//timer
 var timer = 60;
 
 var interval = setInterval(function() {
@@ -7,30 +25,4 @@ var interval = setInterval(function() {
     $('.timer').text(timer);
     if (timer === 0) clearInterval(interval);
 }, 1000);
-
-let installPromptEvent;
-
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome <= 67 from automatically showing the prompt
-  event.preventDefault();
-  // Stash the event so it can be triggered later.
-  installPromptEvent = event;
-  // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
-});
-btnAdd.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+//endtimert
